d2c8303e2207db3418e0ad7a59c73be756ba1192
Leonardo <leonardoboccali19@gmail.com>
2022-01-30 11:50:39 +0100

    created nav element which contain a <ul> and relative <li>. linked the paths to the icon and loop through sidebarData file, which contain the path and the text to display when clicked the icon

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -7,4 +7,39 @@ const Navbar = () => {
     const [sidebar, setSidebar] = useState(false)
 
     const showSidebar = () => setSidebar(!sidebar)
+
+    return (
+        <div className='navbar'>
+            <h1>My Veggie Site</h1>
+
+            <Link to='#' className='menu-bars'>
+                <FaBars className='menu-icon' onClick={showSidebar} />
+            </Link>
+
+            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                <ul className='nav-menu-items' onClick={showSidebar}>
+                    <li className='navbar-toggle'>
+
+                        <Link to='#' className='menu-bars'>
+                            <AiOutlineClose className='menu-icon close' />
+                        </Link>
+
+                    </li>
+
+                    {
+                        SidebarData.map((item, index) => {
+                            return (
+                                <li key={index} className='nav-text-cont'>
+                                    <Link to={item.path} className={item.cName}>
+                                        <span>{item.title}</span>
+                                    </Link>
+                                </li>
+                            )
+                        })
+                    }
+
+                </ul>
+            </nav>
+        </div>
+    )
 }
