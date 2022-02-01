ea66857dd818c69acc1c457037616ea605a0b9ef
Leonardo <leonardoboccali19@gmail.com>
2022-02-01 13:54:10 +0100

    update package.json file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Component } from 'react';
+import { HashRouter, Route, Link } from "react-router-dom";
 import Navbar from './Components/navbar/Navbar'
 import Home from './Components/home/Home'
 import Recipes from './Components/recipes/RecipesList'
@@ -7,26 +9,17 @@ import './App.css';
 function App() {
   return (
     
-      <Router>
+      <HashRouter basename='/'>
         <div className="App">
 
           <Navbar />
 
-          <Switch>
-
-            <Route exact path='/'>
-              <Home />
-            </Route>
-
-            <Route exact path='/recipes'>
-              <Recipes />
-            </Route>
-
-          </Switch>
+            <Route exact path='/' component={Home} />
+            <Route path="/recipes" component={Recipes} />
 
         </div>
 
-      </Router>
+      </HashRouter>
     
   );
 }
